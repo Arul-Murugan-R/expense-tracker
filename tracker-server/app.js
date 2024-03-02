4f3f43143fe89a098df02e6b8c156a9a50747e5a
Arul_Murugan_R <arulmuruganarjun14@gmail.com>
2024-03-02 09:39:32 +0530

    db connection

--- tracker-server/app.js
+++ tracker-server/app.js
@@ -3,10 +3,14 @@ require('dotenv')
 
 const app = express()
 
+const mongoConnect = require('./util/database').mongoConnect;
+
 app.use('/',(req,res)=>{
     res.status(200).json({message:'Everything works fine on the server side'})
 })
 
-app.listen(process.env.PORT||3000,()=>{
-    console.log('listening to http://localhost:3000/')
-})
+mongoConnect(() => {
+    app.listen(process.env.PORT||3000,()=>{
+        console.log('listening to http://localhost:3000/')
+    })
+});
